6edd8b81a9cf9b9a3e3d3b0afe40dff1bfa8f213
Jean <jeanmrtns4@gmail.com>
2020-05-30 19:15:57 -0300

    -

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-$(document).ready(function{
+$(document).ready(function(){
     $("#botao").click(function(){
-        var check = $("#check").is(':checked');
-    })
-})
+
+        //Getting IDs values
+        var nome     = $("#input-nome").val();    
+        var idade    = $("#input-idade").val();   /* Convertendo para inteiro */ idade = parseInt(idade);
+        var endereco = $("#input-endereco").val();     
+        var cpf      = $("#input-cpf").val();
+
+        //Checkboxes
+        var armas           = $("#checkarmas").is(':checked');
+        var dirige          = $("#checkveiculo").is(':checked');
+        var contato_com_ets = $("#checkcontato").is(':checked');
+
+        //From a selector
+        var tipo_sanguineo = $("#tipo-sanguineo").val();
+
+        var dia  = data.getDate();           
+        var mes  = data.getMonth();
+        var ano4 = data.getFullYear();
+
+
+    });
+});
